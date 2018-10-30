d5003efeb95fd8c12d7126a8488ab24e76399fcf
ValentineGladyshko <valentine9022@gmail.com>
2018-10-30 19:55:12 +0200

    Added classes for position and combination (now have only fields and constuctors)

---
+++ DormGrapple/DormGrapple/Combination.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DormGrapple
+{
+    public class Combination
+    {
+        public List<Position> combination;
+
+        public Combination()
+        {
+            combination = new List<Position>();
+        }
+
+        public Combination(List<Position> newCombination)
+        {
+            combination = new List<Position>();
+
+            foreach(var position in newCombination)
+            {
+                combination.Add(new Position(position.Row, position.Column));
+            }
+        }
+    }
+}
---
+++ DormGrapple/DormGrapple/Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DormGrapple
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position()
+        {
+            Row = 0;
+            Column = 0;
+        }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
